6f478989480b0f4ab688d8aad6da32af5ca336a5
Maksim Gorbachewski <gorbachewski.m@gmail.com>
2021-10-26 21:05:05 +0300

    first WebApp

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            // через ViewBag можно передать данные в представление 
+            ViewBag.MyProp = "Hello, World!";
+            // выбор отображаемого представления ( без параметров = по умолчанию, то есть ищется на основе имени файла)
+            return View();
+        }
+
+        // Методы обработки ответов формы
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guest)
+        {
+            // Нужно отправить данные нового гостя no электронной почте 
+            // организатору вечеринки.
+
+            // тут я передаю обьект guest, чтоб использовать его в представлении,
+            // также я мог продублировать данные в ViewBag
+            if (ModelState.IsValid)
+                return View("Thanks", guest);
+            else
+                return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ Web/Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Web.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Пожалуйста, введите свое имя")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Вы ввели некорректный email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите телефон")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, укажите, примите ли участие в вечеринке")]
+        public bool? WillAttend { get; set; }
+    }
+}
